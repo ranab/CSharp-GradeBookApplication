1500fef8b88bb4ee25761a6c4960b3c71756fabb
ranab <ranjuabraham@gmail.com>
2018-12-01 23:13:35 +0000

    Committed the module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -9,6 +10,40 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (this.Students.Count < 5)
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+
+            var threshold = (int)Math.Ceiling(Students.Count * .2);
+
+            var grades = Students.OrderByDescending(s => s.AverageGrade).Select(s => s.AverageGrade).ToList();
+
+            if (grades[threshold - 1] <= averageGrade)
+                return 'A';
+            else if (grades[threshold * 2 - 1] <= averageGrade)
+                return 'B';
+            else if (grades[threshold * 3 - 1] <= averageGrade)
+                return 'C';
+            else if (grades[threshold * 4 - 1] <= averageGrade)
+                return 'D';
+            else if (grades[threshold * 5 - 1] <= averageGrade)
+                return 'E';
+
+            return 'F';
+        }
+
+        private void GetPerOfStudentsWithGradeGreaterThan(char g)
+        {
+            var higherGradeStudents = this.Students.Where(s => s.LetterGrade < g).Count();
+            decimal highPerStudents = higherGradeStudents / Students.Count;
+
+            
+                    
+
+              
         }
     }
 }
